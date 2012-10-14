f4549c7a8f4b2dd7f917855c473998f522fd1c68
Sergey Krivich <dvdking@i.ua>
2012-10-15 00:45:52 +0300

    simple level generation visualisation

--- DN/Game.cs
+++ DN/Game.cs
@@ -29,16 +29,21 @@ namespace DN
     {
         #region GLOBAL
 
-        // public static Size g_screenSize = new Size(1080, 1920);
+       //  public static Size g_screenSize = new Size(1080, 1920);
       //  public static Rectangle g_screenRect = new Rectangle(0, 0, 1080, 1920);
+
+       // public static Size g_screenSize = new Size(1920, 1080);
+    //    public static Rectangle g_screenRect = new Rectangle(0, 0, 1920, 1080);
+
         public static Size g_screenSize = new Size(640, 480);
-        public static Rectangle g_screenRect = new Rectangle(0, 0, 640, 480);
+       public static Rectangle g_screenRect = new Rectangle(0, 0, 640, 480);
 
         public static KeyboardDevice g_Keyboard;
         public static MouseDevice g_Mouse;
         public static GamepadState g_Gamepad;
         #endregion
         private GameWorld gameWorld;
+        private LevelGenerator levelGenerator;
         AudioContext audioContext;
         AudioClip clip;
         BitmapFont font;
@@ -46,9 +51,13 @@ namespace DN
             : base(g_screenSize.Width, g_screenSize.Height, GraphicsMode.Default, "Devil's nightmare")
         {
             GraphicsDevice.Instance.Initialize(g_screenSize.Width, g_screenSize.Height);
-
             VSync = VSyncMode.On;
-            
+
+        }
+
+        private void GKeyboardOnKeyDown(object sender, KeyboardKeyEventArgs e)
+        {
+            levelGenerator.Skip = true;
         }
 
         protected override void OnLoad(EventArgs e)
@@ -60,14 +69,13 @@ namespace DN
             g_Mouse = Mouse;
             g_Gamepad = new GamepadState(GamepadIndex.One);
 
-            gameWorld = new GameWorld(100, 100);
-            Thread worldCreationThread = new Thread(CreateWorld);
+            g_Keyboard.KeyDown += GKeyboardOnKeyDown;
 
-            worldCreationThread.Start();
+
 
             audioContext = new AudioContext();
             LoadContent();
-
+            CreateWorld();
             new DiagnosticsCenter();
 
             font = new BitmapFont(new Font("Consolas", 14));
@@ -80,10 +88,11 @@ namespace DN
             AudioClip clip = new AudioClip(Path.Combine("Content", "Sounds", "rainfall.ogg"));
             clip.Play();
 
-            while(worldCreationThread.IsAlive)
-            {
-                Thread.Sleep(1);
-            }
+
+        }
+
+        private void OnFinishGeneration()
+        {
             gameWorld.InsertHero();
             gameWorld.InitGui();
             for (int i = 0; i < 0; i++)
@@ -96,8 +105,8 @@ namespace DN
 
         private void CreateWorld()
         {
-
-            var lg = new LevelGenerator
+            gameWorld = new GameWorld(100, 100);
+            levelGenerator = new LevelGenerator
             {
                 RoomsMaxWidth = 10,
                 RoomsMaxHeight = 15,
@@ -105,7 +114,8 @@ namespace DN
                 Scale = 0.5f,
                 WallSmoothing = 0.5f
             };
-            lg.Generate(gameWorld);
+            levelGenerator.GenerationFinishedEvent += OnFinishGeneration;
+            levelGenerator.Generate(gameWorld);
         }
 
         private void LoadContent()
@@ -120,7 +130,9 @@ namespace DN
             CM.I.LoadTexture("potion", Path.Combine("Content", "Textures", "GameObjects", "Potion.png"));
             CM.I.LoadTexture("arrow_sprite", Path.Combine("Content", "Textures", "Weapons", "arrow.png"));
             CM.I.LoadTexture("bow_sprite", Path.Combine("Content", "Textures", "Weapons", "Bow.png"));
-
+            CM.I.LoadTexture("mini_wall", Path.Combine("Content", "Textures", "LevelGeneration", "mini_wall.png"));
+            CM.I.LoadTexture("mini_ladder", Path.Combine("Content", "Textures", "LevelGeneration", "mini_ladder.png"));
+            CM.I.LoadTexture("mini_miner", Path.Combine("Content", "Textures", "LevelGeneration", "mini_miner.png"));
 
             CM.I.LoadFont("Big", Path.Combine("Content", "Fonts", "monofur.ttf"), 48);
             CM.I.LoadFont("Middle", Path.Combine("Content", "Fonts", "monofur.ttf"), 24);
@@ -134,27 +146,43 @@ namespace DN
 
         protected override void OnUpdateFrame(FrameEventArgs e)
         {
+            float dt = (float)e.Time;
             if (g_Keyboard[Key.Escape])
                 Exit();
-            if (g_Keyboard[Key.Tilde]) if (DiagnosticsCenter.Instance.Visible) DiagnosticsCenter.Instance.Hide(); else DiagnosticsCenter.Instance.Show();
-            float dt = (float) e.Time;
-            g_Gamepad.Update(dt);
-            gameWorld.Update(dt);
-            DiagnosticsCenter.Instance.Update(dt);
+            if (!levelGenerator.Finished)
+            {
+                levelGenerator.Update(dt);
+                goto exit;
+            }
+
+            if (g_Keyboard[Key.Tilde])
+                    if (DiagnosticsCenter.Instance.Visible) DiagnosticsCenter.Instance.Hide();
+                    else DiagnosticsCenter.Instance.Show();
+
+                g_Gamepad.Update(dt);
+                gameWorld.Update(dt);
+                DiagnosticsCenter.Instance.Update(dt);
+         exit:   
             base.OnUpdateFrame(e);
         }
         
         protected override void OnRenderFrame(FrameEventArgs e)
         {
             GL.Clear(ClearBufferMask.ColorBufferBit);
-            
-         //   SpriteBatch.Instance.Begin();
-        //    SpriteBatch.Instance.OutlineRectangle(new RectangleF(5,5,10,40),  Color.White, 10f, 1f, new Vector2(0.5f, 0.5f));
-         //   SpriteBatch.Instance.End();
             float dt = (float)e.Time;
-            gameWorld.Draw(dt);
-            DiagnosticsCenter.Instance.Draw(dt);
+            if (levelGenerator.Finished)
+            {
+                gameWorld.Draw(dt);
+                DiagnosticsCenter.Instance.Draw(dt);
 
+
+            }
+            else
+            {
+                SpriteBatch.Instance.Begin();
+                levelGenerator.Draw(dt);
+                SpriteBatch.Instance.End();
+            }
             SwapBuffers();
             base.OnRenderFrame(e);
 
